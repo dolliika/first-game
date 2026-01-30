b7379be4a5da8e871bf0f6537085e279af2dd29d
Lan Nguyen <str4wbxrryymilk@gmail.com>
2026-01-30 20:14:09 +0700

    implemented leaderstats and coin logic

---
+++ src/server/coins.server.lua
@@ -0,0 +1,22 @@
+local Players = game:GetService("Players")
+local coins = workspace.Coins:GetChildren()
+
+for _, coin in ipairs(coins) do
+  coin:SetAttribute("Collected", false)
+
+  coin.Touched:Connect(function(otherPart)
+    if coin:GetAttribute("Collected") then return end
+
+    local character = otherPart.Parent
+    local humanoid = character:FindFirstChild("Humanoid")
+    local player = Players:GetPlayerFromCharacter(character)
+    if not humanoid or not player then return end
+    
+    local coinsStat = player:WaitForChild("leaderstats"):WaitForChild("Coins")
+    coinsStat.Value += math.random(1, 5)
+
+    coin:SetAttribute("Collected", true)
+    coin.Transparency = 0.8
+    coin.CanTouch = false
+  end)
+end
